2410d7d8dc553db990b341a1c7a7682d4b833c67
grachevaa01 <grachevaa01@mail.ru>
2023-01-13 14:01:51 +0300

    Сделано ДЗ

---
+++ src/Main.java
@@ -0,0 +1,59 @@
+public class Main { //
+
+    public static void main (String[] args) { //
+   var dog = 8.0;
+    System.out.println(dog);
+    dog = dog + 4;
+         System.out.println(dog);
+         dog = dog - 3.5;
+                 System.out.println(dog);
+    var cat = 3.6;
+    System.out.println(cat);
+    cat = cat + 4;
+    System.out.println(cat);
+    cat = cat - 1.6;
+    System.out.println(cat);
+    var paper = 763789;
+    System.out.println(paper);
+    paper = paper + 4;
+    System.out.println(paper);
+    paper = paper - 7639;
+    System.out.println(paper);
+    var friend = 19;
+    System.out.println(friend);
+    friend = friend + 2;
+    System.out.println(friend);
+    friend = friend / 7;
+    System.out.println(friend);
+    var frog = 3.5;
+    frog = frog * 10;
+    System.out.println(frog);
+    frog = frog / 3.5;
+    System.out.println(frog);
+    frog = frog +4;
+    System.out.println(frog);
+    var firstBoxer = 78.2;
+    System.out.println(firstBoxer);
+    var secondBoxer = 82.7;
+    System.out.println(82.7);
+    var stuffWeight = firstBoxer + secondBoxer;
+    System.out.println("Общий вес" + stuffWeight);
+    var differenceWeight = secondBoxer - firstBoxer;
+    System.out.println("Разница в весе" + differenceWeight);
+    var overload = (firstBoxer - secondBoxer) %stuffWeight;
+    System.out.println("Разница в " + overload + "кг !");
+    var totalHours = 640;
+    System.out.println(totalHours);
+    var oneEmployee = 8;
+    System.out.println(oneEmployee);
+    var totalEmployee = (totalHours / oneEmployee);
+    System.out.println("Всего работников в компании " + totalEmployee + " человек");
+    var otherCompany = totalEmployee + 94;
+    System.out.println(otherCompany);
+    var otherTotalHours = otherCompany * 8;
+    System.out.println("Если в компании работает " + otherCompany + " человек, то всего " + otherTotalHours + " часов работы может быть поделено между сотрудниками");
+
+
+
+    }
+}
