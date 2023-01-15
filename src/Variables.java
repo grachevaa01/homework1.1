dc56d37bba6e1942b6990c846726d4167cee38ea
grachevaa01 <grachevaa01@mail.ru>
2023-01-15 17:49:48 +0300

    Сделано ДЗ

---
+++ src/Variables.java
@@ -0,0 +1,110 @@
+public class Variables {//
+    public static void main ( String[] args )
+    {//
+        int oranges = 5;
+        System.out.println("Значение переменной oranges "  + "с типом int " + "равно " + oranges);
+        byte blueberry = 12;
+        System.out.println("Значение переменной blueberry " + "с типом byte " + "равно " + blueberry);
+        short box = 200;
+        System.out.println("Значение переменной box с типом short равно " + box);
+        long watterBottle = 4280;
+        System.out.println("Значение переменной watterBottle с типом long равно " + watterBottle);
+        float marketWeight = 8644;
+        System.out.println("Значение переменной marketWeight с типом flot равно " + marketWeight);
+        double carWeight = 3000.5;
+        System.out.println("Значение переменной carWeight с типом double равно " + carWeight);
+        byte firstValue = 67;
+        System.out.println(firstValue);
+        int secondValue = 159;
+        System.out.println(secondValue);
+        short thirdValue = 569;
+        System.out.println(thirdValue);
+        long fourthValue = 27897;
+        System.out.println(fourthValue);
+        float fifthValue = 987678;
+        System.out.println(fifthValue);
+        float sixthValue = 965549;
+        System.out.println(sixthValue);
+        double seventhValue = 2.786;
+        System.out.println(seventhValue);
+        double eighthValue = 27.12;
+        System.out.println(eighthValue);
+        int firstClass = 23;
+        System.out.println(firstClass);
+        int secondClass = 27;
+        System.out.println(secondClass);
+        int thirdClass = 30;
+        System.out.println(thirdClass);
+        short totalPaper = 480;
+        System.out.println(totalPaper);
+        long totalClasses = (firstClass + secondClass + thirdClass);
+                System.out.println(totalClasses);
+       long forOneClass = (totalPaper / 3);
+        System.out.println(forOneClass);
+        long forEveryone = (forOneClass / totalClasses);
+        System.out.println("На каждого ученика рассчитано " + forEveryone + " листов бумаги");
+        int bottlesTwenty = (20 * 8);
+        System.out.println("За 20 минут машина произведет " + bottlesTwenty + " бутылок");
+        int bottlesDay = ( 1440 * 8);
+        System.out.println("За сутки машина произведет " + bottlesDay + " бутылок");
+        int bottlesThreeDays = (bottlesDay * 3);
+        System.out.println("За 3 дня машина произведет " + bottlesThreeDays + " бутылок");
+        double bottlesMonth = (bottlesDay * 30.5);
+        System.out.println("За месяц машина произведет " + bottlesMonth + " бутылок");
+        byte sumClasses = (120 / 6);
+        System.out.println(sumClasses);
+        byte brownColor = (20 * 4);
+        System.out.println(brownColor);
+        byte whiteColor = (20 * 2);
+        System.out.println(whiteColor);
+        System.out.println("В школе, где " + sumClasses + " классов, нужно " + whiteColor + " банок белой краски и " + brownColor + " банок коричневой краски");
+        int bananas = ( 5 * 80);
+        System.out.println(bananas);
+        int milk = (200 * 105);
+        System.out.println(milk);
+        int iceCream = (100 * 2);
+        System.out.println(iceCream);
+        int eggs = (4 * 70);
+        System.out.println(eggs);
+        int totalInGrm = (bananas + milk + iceCream + eggs);
+        System.out.println("Вес такого завтрака в граммах " + totalInGrm);
+        int totalInKg = (totalInGrm / 1000);
+        System.out.println("Вес такого завтрака в киллограммах " + totalInKg);
+        int ifMinimum = (7000 / 250);
+        System.out.println("Если спортсмен будет сбрасывать в день по 250 гр, то ему понадобится " + ifMinimum + " дней");
+        int ifAverage = (7000 / 500);
+        System.out.println("Если спортсмен будет сбрасывать в день по 500 гр, то ему понадобится " + ifAverage + " дней");
+        byte inMiddle = (7000 / 350);
+        System.out.println("В среднем спортсмену понадобится " + inMiddle  + " дней, чтобы похудеть на 7 кг");
+        double glowUpMaria = (67.760 / 10);
+        System.out.println(glowUpMaria);
+        double maria = (67.760 + glowUpMaria);
+        System.out.println("Маша теперь получает " + maria + " рублей. Годовой доход вырос на " + glowUpMaria);
+        double glowUpDenis = (83.690 / 10);
+        System.out.println(glowUpDenis);
+        double denis = (83.690 + glowUpDenis);
+        System.out.println("Денис теперь получает " + denis + " рублей. Годовой доход вырос на " + glowUpDenis);
+        double glowUpChristina = ( 76.230 / 10);
+        System.out.println(glowUpChristina);
+        double christina = (92.059 + glowUpChristina);
+        System.out.println("Кристина теперь получает " + christina + "рублей. Годовой доход вырос на " + glowUpChristina);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         }//
+
+
+}//
